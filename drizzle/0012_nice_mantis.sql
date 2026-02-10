0c4d64458bba75b93cc97616902f780c191671ce
pratham-ak2004 <pratham20442@gmail.com>
2026-02-10 16:49:42 +0530

    [fix] migration mismatch between e1b2f5c4d4de88c8b5e77c01d208545cb6b3fd69 and 8aad008f05c8701005abd5517901adc282e2d4ad

--- drizzle/0012_nice_mantis.sql
+++ drizzle/0012_nice_mantis.sql
@@ -1,6 +1,5 @@
 CREATE TABLE "site-settings" (
 	"id" text PRIMARY KEY NOT NULL,
-	"registrations_open" boolean DEFAULT false,
 	"results_out" boolean DEFAULT false,
 	"payment_open" boolean DEFAULT false
 );
